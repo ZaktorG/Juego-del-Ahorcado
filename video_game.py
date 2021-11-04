93e73e9fce67e35a26869452c7875cede8013b6c
Rodrigo Baez Aguilar <baezaguiro@gmail.com>
2021-11-03 18:53:45 -0600

    Se agrega una función read_file
    Esta función nos sirve para leer los datos de nuestro archivo ubicado
    en la carpeta source/data.txt y retorna la lista

--- video_game.py
+++ video_game.py
@@ -1,5 +1,12 @@
+def read_file():
+    word = []
+    with open("./source/data.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            word.append(line.strip("\n"))
+    return word
+
 def run():
-    pass 
+    read_file()  
 
 if __name__ == "__main__":
     run()
