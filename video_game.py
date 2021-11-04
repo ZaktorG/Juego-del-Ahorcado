2957fc348b2dcafec15bc650a302e745efc5ba6e
Rodrigo Baez Aguilar <baezaguiro@gmail.com>
2021-11-04 17:30:34 -0600

    Se programo la funcionalidad del juego
    Solo falta darle estilo al juego, ya que esta programada la funcionalidad

--- video_game.py
+++ video_game.py
@@ -1,6 +1,8 @@
 import os
 import random
 
+from functools import reduce
+
 TITLE_GAME = """       _                              _      _            _                             _       
       | |                            | |    | |     /\   | |                           | |      
       | |_   _  ___  __ _  ___     __| | ___| |    /  \  | |__   ___  _ __ ___ __ _  __| | ___  
@@ -39,10 +41,40 @@ TITLE_GAME_ERROR = """       _                              _      _            
 
 *Favor de ingresar una opción valida*
 Ingrese su elección: """
+def game(word_game, word_size):
+    os.system("clear")
+    # print(word_game)
+    vidas = 5
+    word_game_result = ""
+    words_input = []
+    for i in range(word_size):
+        words_input.append("_")
+    
+    while vidas > 0 and word_game != word_game_result:
+        word_search = False
+        word_input = input("Ingrese una letra: ")
+        for value in range(word_size):
+            if word_input == word_game[value]:
+                words_input[value] = word_input
+                word_search = True
+        if not word_search:
+            vidas -= 1
+        # Solo falta reducir la palabra a un solo string para comparar
+        word_game_result = reduce(lambda a, b: a + b, words_input)
+        print(words_input, vidas, word_game_result)
+    if word_game == word_game:
+        return True
+    else:
+        return False
 
-def random_word(word, word_size):
-    number_random = random.randint(0, word_size) 
-    return word.get(number_random)
+def random_word(word, words_size):
+    number_random = random.randint(0, words_size) 
+    word = word.get(number_random)
+
+    word_size = 0
+    for value in word:
+        word_size += 1
+    return  word, word_size
     
 def read_file():
     words = {}
@@ -54,21 +86,23 @@ def read_file():
     return words, i
 
 def run():
+    words, words_size = read_file()
     os.system("clear")
-    choice_menu = input(TITLE_GAME)
-
-    words, word_size = read_file()
-    word_game = random_word(words, word_size)  
-
-    if choice_menu == "1":
-        pass 
-    elif choice_menu == "2":
-        pass 
-    elif choice_menu == "3":
-        pass 
-    else:
-        os.system("clear")
-        choice_menu = input(TITLE_GAME_ERROR)
+    choice_menu = input(TITLE_GAME) 
+    while choice_menu != "3":
+        if choice_menu == "1":
+            word_game, word_size = random_word(words, words_size) 
+            if game(word_game, word_size):
+                os.system("clear")
+                print("Ganaste")
+            choice_menu = input("\nstop")
+        elif choice_menu == "2":
+            pass 
+        elif choice_menu == "3":
+            pass 
+        else:
+            os.system("clear")
+            choice_menu = input(TITLE_GAME_ERROR)
         
 
 if __name__ == "__main__":
