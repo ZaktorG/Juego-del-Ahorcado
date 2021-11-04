40877c06ef182fce0365199ebaf126f1927b01f7
Rodrigo Baez Aguilar <baezaguiro@gmail.com>
2021-11-03 19:22:56 -0600

    Modificación en la funcion read y se agrega otra random
    La modificación en la función read_file es que antes retornaba una lista
    y para hacer mas sencillo el codigo siguiente decidi que donde se
    almacenara fuera un diccionario.
    Se agrega la funcion random_word la cual escoge una palabra al azar del
    diccionario, aparte se importa random.

--- video_game.py
+++ video_game.py
@@ -1,12 +1,22 @@
+import random
+
+def random_word(word, word_size):
+    number_random = random.randint(0, word_size) 
+    print(str(word.get(number_random)))
+    
+
 def read_file():
-    word = []
+    word = {}
     with open("./source/data.txt", "r", encoding="utf-8") as f:
+        i = 0
         for line in f:
-            word.append(line.strip("\n"))
-    return word
+            word[i] = line.strip("\n")
+            i += 1
+    return word, i
 
 def run():
-    read_file()  
+    word, word_size = read_file()
+    random_word(word, word_size)  
 
 if __name__ == "__main__":
     run()
